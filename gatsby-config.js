95c7c62fcf833a5ed78cb8ae5c68634609e4035f
Guillermo Echague <guillermo.echaguea@gmail.com>
2021-04-15 06:41:12 -0400

    Cambio de configuracion

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,12 +21,12 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
-        short_name: `starter`,
+        short_name: `cvGE`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/cv-icon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-offline`,
