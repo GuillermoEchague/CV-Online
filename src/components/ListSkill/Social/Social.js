6c97865541260fefe4c7d09075ff9ebff52c003b
Guillermo Echague <guillermo.echaguea@gmail.com>
2021-04-22 05:14:39 -0400

    Social media - Github - link

--- src/components/ListSkill/Social/Social.js
+++ src/components/ListSkill/Social/Social.js
@@ -1,33 +1,31 @@
 import React from 'react'
-import IcTwitter from "../../../images/icons/twitter.svg"
-import IcGithub from "../../../images/icons/github.svg"
-import IcLinkedin from "../../../images/icons/linkedin.svg"
+
 import { Image} from "react-bootstrap"
 import "./Social.scss"
 
 export default function Social() {
     return (
-      <div class="social">
+      <div class="link">
       
       <a 
       href= "https://www.linkedin.com/in/guillermo-echag%C3%BCe-arriaza-a198b2192/"
       target="_blank" 
       rel="noreferrer noopener">
-      <Image height={30} src="https://github.com/GuillermoEchague/GuillermoEchague/blob/main/img/linkedin.svg" />
+      <Image height={30} src="https://raw.githubusercontent.com/GuillermoEchague/GuillermoEchague/7e7f4a38d747f5d66c944714343ecd6c2cebba46/img/linkedin.svg" />
       </a>
       
       <a 
       href= "https://github.com/GuillermoEchague"
       target="_blank" 
       rel="noreferrer noopener">
-      <Image height={30} src="https://github.com/GuillermoEchague/GuillermoEchague/blob/main/img/github.svg" />
+      <Image height={30} src="https://raw.githubusercontent.com/GuillermoEchague/GuillermoEchague/7e7f4a38d747f5d66c944714343ecd6c2cebba46/img/github.svg" />
       </a>
       
       <a 
       href= "https://twitter.com/GuillermoEchag1"
       target="_blank" 
       rel="noreferrer noopener">
-      <Image height={30} src="https://github.com/GuillermoEchague/GuillermoEchague/blob/main/img/twitter.svg" />
+      <Image height={30} src="https://raw.githubusercontent.com/GuillermoEchague/GuillermoEchague/7e7f4a38d747f5d66c944714343ecd6c2cebba46/img/twitter.svg" />
       </a>
       </div>
       
